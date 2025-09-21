252e2297debd9c9069809296139a1abf0ca98ca9
Juwoojae <juwqq1234@naver.com>
2025-09-22 02:14:03 +0900

    요구사항 추가

---
+++ src/main/java/hello/itemservice/domain/item/ItemType.java
@@ -0,0 +1,15 @@
+package hello.itemservice.domain.item;
+
+public enum ItemType {
+    BOOK("도서"), FOOD("식품"), ETC("기타");
+
+    private final String description; //설명
+
+    ItemType(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
